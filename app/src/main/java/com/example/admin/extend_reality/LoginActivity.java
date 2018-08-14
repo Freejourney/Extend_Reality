c0d81e2dac2ba92cd21f3ed837bad939d1624cef
lonelyjourney <983621131@qq.com>
2018-08-14 14:39:38 +0800

    add the loginactivity

--- app/src/main/java/com/example/admin/extend_reality/LoginActivity.java
+++ app/src/main/java/com/example/admin/extend_reality/LoginActivity.java
@@ -3,12 +3,23 @@ package com.example.admin.extend_reality;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
+
+    private EditText et_passwd;
+    private EditText et_username;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initview();
+    }
+
+    private void initview() {
+        et_username = findViewById(R.id.username_et);
+        et_passwd = findViewById(R.id.password_et);
     }
 }
