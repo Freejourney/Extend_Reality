e2d6712f992866353ce20523127f722f723c2de3
lonelyjourney <983621131@qq.com>
2018-08-22 17:09:29 +0800

    add User

--- app/src/main/java/com/example/admin/extend_reality/User.java
+++ app/src/main/java/com/example/admin/extend_reality/User.java
@@ -1,4 +1,16 @@
 package com.example.admin.extend_reality;
 
-public class User {
+import cn.bmob.v3.BmobUser;
+
+public class User extends BmobUser {
+    private String Avatorurl;
+
+    public String getAvatorurl() {
+        return Avatorurl;
+    }
+
+    public void setAvatorurl(String avatorurl) {
+        Avatorurl = avatorurl;
+    }
+
 }
